9482fab4a31da8b48ddcb3f0a16a90b7e2b7417e
AFK aka Akos Ferenc Kalamar <akos.kalamar@gmail.com>
2020-01-08 20:03:33 +0100

    add property and event to favorite component, then take some style

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  styles: [
+    `
+      .fa-star {
+        color: green;
+      }
+    ` ]
 })
-export class FavoriteComponent implements OnInit {
-  isFavorite: boolean;
-
-  constructor() { this.isFavorite = false; }
-
-  ngOnInit() {
-  }
+export class FavoriteComponent {
+  @Input('is-selected') isSelected: boolean;
+  @Output('change') click = new EventEmitter();
 
   onClick() {
-    this.isFavorite = !this.isFavorite;
+    this.isSelected = !this.isSelected;
+    this.click.emit({ newValue: this.isSelected });
   }
+}
+
+export interface FavoriteChangeEventArgs {
+  newValue: boolean;  
 }
