beeb90911ef9ace9a116286f6eee82e894666eb5
AFK aka Akos Ferenc Kalamar <akos.kalamar@gmail.com>
2020-01-10 20:56:38 +0100

    section 6 tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,11 @@ import { FavoriteChangeEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  examples
+  canSave = true; 
+
+  viewMode = 'map';
+  courses = [1,2];
   post = {
     title: "Title",
     isFavorite: true
@@ -18,7 +23,37 @@ export class AppComponent {
     likesCount: 40
   }
 
+  task = {
+    title: 'Review application',
+    assignee: null
+  }
+
   onFavoriteChange(eventArgs: { newValue: FavoriteChangeEventArgs }) {
     console.log("favorite change to: ", eventArgs);
+  }
+
+  onAdd() {
+    this.examples.push( {id: 4, name: 'example 4'});
+  }
+
+  onRemove(example) {
+    let index = this.examples.indexOf(example);
+    this.examples.splice(index, 1);
+  }
+
+  onChange(example) {
+    example.name = 'UPDATED';
+  }
+
+  loadExamples() {
+    this.examples = [
+      {id: 1, name: 'example 1 '},
+      {id: 2, name: 'example 2 '},
+      {id: 3, name: 'example 3 '},
+    ];
+  }
+
+  trackExample(index, example){
+    return example ? example.id : undefined;
   }
 }
